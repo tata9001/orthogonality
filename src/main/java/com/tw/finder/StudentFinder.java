c754b7435e810e2f7d8bab3e2820e91e4f2d9d21
Yun Wang <yun.wang@rea-group.com>
2016-01-14 22:30:26 +0800

    5.refactor with  Iterable.

--- src/main/java/com/tw/finder/StudentFinder.java
+++ src/main/java/com/tw/finder/StudentFinder.java
@@ -3,11 +3,10 @@ package com.tw.finder;
 import com.tw.finder.po.Student;
 import com.tw.finder.predicate.StudentPredicate;
 
-import java.util.ArrayList;
 
 public class StudentFinder {
 
-    public static Student find(ArrayList<Student> students, StudentPredicate p) {
+    public static Student find(Iterable<Student> students, StudentPredicate p) {
 
         for (Student student : students) {
             if (p.test(student)) {
